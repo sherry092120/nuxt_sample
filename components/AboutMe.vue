d5bc053e895c7bf1fcf524782380b78fe295611b
sherry092120 <airkingdom092120@gmail.com>
2023-11-19 23:32:49 +0800

    打包測試

--- components/AboutMe.vue
+++ components/AboutMe.vue
@@ -7,8 +7,8 @@
             </div>
             <div class="content-block content" v-html="content"></div>
             <div class="images-block">
-                <img class="image-bg" :src="$imagePath+'bg_orange_684x534.png'">
-                <img class="image-cat" :src="$imagePath+'cat_658x658.png'">
+                <img class="image-bg" src="@/assets/image/bg_orange_684x534.png">
+                <img class="image-cat" src="@/assets/image/cat_658x658.png">
                 <div class="title name">喵立翰</div>
                 <div class="en name_en">Miao Li-Han</div>
             </div>
@@ -17,7 +17,6 @@
 </template>
   
 <script setup>
-const { $imagePath } = useNuxtApp();
 const content = ref('我堅信，藉由推動更完善的貓咪福利和相關政策，更是間接地投資於台灣的未來。畢竟，民眾的身心健康與工作熱情是推動經濟的核心動力。透過完善的貓咪福利政策，為台灣的 GDP 經濟帶來巨大效益。<br>因此，我期望能在立法院內推進這些政策，確保每一隻貓咪都能得到他們應有的照顧，同時也為台灣的經濟發展助一臂之力。讓我們一同護航台灣的幸福經濟，從照顧每一隻貓咪開始。')
 
 </script>
--- plugins/provide.js
+++ plugins/provide.js
@@ -3,7 +3,8 @@ import { defineNuxtPlugin } from "#app"
 export default defineNuxtPlugin(() => {
   return {
     provide: {
-        imagePath: "http://localhost:3000/_nuxt/assets/image/",
+        // imagePath: "http://localhost:3000/_nuxt/assets/image/",
+        imagePath: "https://sherry092120.github.io/nuxt_sample/assets/image/",
     },
   }
 })
